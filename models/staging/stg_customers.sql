cbe2e8d6244f063da2b2912190d0ff86d5008ccc
vishal <lakhmapurkar.vishal@gmail.com>
2026-06-28 17:27:01 +0200

    Update stg_customers.sql

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,15 +1,7 @@
 {{ config(materialized='view') }}
 
-with raw_customers as (
-    select
-        customer_id,
-        email,
-        created_at
-    from {{ source('raw', 'customers') }}
-)
-
 select
     customer_id,
-    email,
-    created_at
-from raw_customers
+    email
+from {{ ref('raw_customers') }}
+where is_deleted = false
